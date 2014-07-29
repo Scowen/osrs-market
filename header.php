4809fc9bdf2af2a88c89b2492289e08f02d3a515
Luke Scowen <lmscowen@gmail.com>
2014-07-29 12:18:57 +0100

    Base Commit. (Details)
    Composer Config.
    Header, Footer and Index.
    Main CSS.
    Main JS.
    .gitIgnore for the Composer directory.

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf8" />
+        <!-- Import Jquery -->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <!-- Scale the UI dependant on device via TWBS -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <!-- Import TWBS JS and CSS -->
+        <script src="assets/vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+        <link  href="assets/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+
+        <!-- Import custom utils -->
+        <link  href="assets/css/main.css" rel="stylesheet" type="text/css" media="all" />
+        <script src="assets/js/main.js"></script>
+
+        <!-- Import Google Fonts -->
+        <link  href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
+        <link  href='http://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+
+        <title>
+            OSRS Market - Price Statistics and More!
+        </title>
+    </head>
+    <body>
+        <div class="navigation text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4 link">Link</div>
+                    <div class="col-sm-4">OSRS Market</div>
+                    <div class="col-sm-4 link">Link</div>
+                </div>
+            </div>
+        </div>
+
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Include the header page which includes the main layout CSS, JS etc.
+include("header.php");
+?>
+
+
+
+<?php
+// Include the footer to end main html tags.
+include("footer.php");
